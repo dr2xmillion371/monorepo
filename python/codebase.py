975992e74f438451859d4dca70702ad1eb882d6c
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-05-07 19:57:59 -0500

    Create codebase.py

---
+++ python/codebase.py
@@ -0,0 +1,146 @@
+# START
+
+
+
+
+""" this codebase file is just made out of comments """
+
+
+# Users:
+# Unknown user 1
+# Unknown user 2
+# Unknown user 3
+# Unknown user 4
+# Unknown user 5
+# Unknown user 6
+
+
+# Unique users:
+# Unknown unique user 1
+# Unknown unique user 2
+# Unknown unique user 3
+# Unknown unique user 4
+# Unknown unique user 5
+# Unknown unique user 6
+# Unknown unique user 7
+# Unknown unique user 8
+# Unknown unique user 9
+# Unknown unique user 10
+# Unknown unique user 11
+# Unknown unique user 12
+# Unknown unique user 13
+# Unknown unique user 14
+# Unknown unique user 15
+# Unknown unique user 16
+# Unknown unique user 17
+# Unknown unique user 18
+# Unknown unique user 19
+# Unknown unique user 20
+# Unknown unique user 21
+# Unknown unique user 22
+# Unknown unique user 23
+# Unknown unique user 24
+# Unknown unique user 25
+# Unknown unique user 26
+# Unknown unique user 27
+# Unknown unique user 28
+# Unknown unique user 29
+# Unknown unique user 30
+# Unknown unique user 31
+# Unknown unique user 32
+# Unknown unique user 33
+# Unknown unique user 34
+# Unknown unique user 35
+# Unknown unique user 36
+# Unknown unique user 37
+
+
+
+
+
+# Viewers:
+# Viewer 1
+# Viewer 2
+# Viewer 3
+# Viewer 4
+# Viewer 5
+# Viewer 6
+# Viewer 7
+# Viewer 8
+# Viewer 9
+# Viewer 10
+# Viewer 11
+# Viewer 12
+# Viewer 13
+# Viewer 14
+# Viewer 15
+# Viewer 16
+# Viewer 17
+# Viewer 18
+# Viewer 19
+# Viewer 20
+# Viewer 21
+# Viewer 22
+# Viewer 23
+# Viewer 24
+# Viewer 25
+# Viewer 26
+# Viewer 27
+
+
+
+
+# Unique viewers:
+# Unique viewer 1
+# Unique viewer 2
+# Unique viewer 3
+# Unique viewer 4
+# Unique viewer 5
+# Unique viewer 6
+# Unique viewer 7
+# Unique viewer 8
+# Unique viewer 9
+# Unique viewer 10
+# Unique viewer 11
+# Unique viewer 12
+# Unique viewer 13
+# Unique viewer 14
+# Unique viewer 15
+# Unique viewer 16
+
+
+
+
+"""
+Database-related section 
+"""
+
+
+# Store users
+# Store all users
+
+
+
+# Store unique users
+# Store all unique users
+
+
+# Store viewers
+# Store all viewers
+
+
+# Store unique viewers
+# Store all unique viewers
+
+
+# Save Database
+# Save all progress for database
+
+
+
+""" end of Database-related section """
+
+
+
+
+# END
